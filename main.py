ea5b733a2b59a0197d31cf1519695865eb5537db
KlebersonTobias <kleberson955j@gmail.com>
2026-04-19 00:30:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from tkinter import *
 from tkinter import ttk
 
+cor00 = "#000000"
+
 janela = Tk()
 janela.title("Calculadora")
 janela.geometry("235x318")
+janela.config(background=cor00)
+
+frameTela = Frame(janela, width=235, height=50)
+frameTela.grid(row=0, column=0)
+
 
 janela.mainloop()
