e27bca954f350fb2905219dd9f37e726febdb838
KlebersonTobias <kleberson955j@gmail.com>
2026-04-20 01:04:51 -0300

    alteração-estrutura
    alterando toda lógica e estrutura para rodar o docker

--- main.py
+++ main.py
@@ -1,19 +1,2 @@
-from tkinter import *
-from tkinter import ttk
-
-cor00 = "#000000"
-cor01 = "#666a78"
-
-janela = Tk()
-janela.title("Calculadora")
-janela.geometry("235x318")
-janela.config(background=cor00)
-
-frameTela = Frame(janela, width=235, height=50, background=cor01)
-frameTela.grid(row=0, column=0)
-
-frameCorpo = Frame(janela, width=235, height=268)
-frameCorpo.grid(row=1, column=0)
-
-
-janela.mainloop()
+def iniciar():
+    print("Lógica da aplicação rodando")
