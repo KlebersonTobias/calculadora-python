68922abdbd87932b71e3da1685454e535f928f68
KlebersonTobias <kleberson955j@gmail.com>
2026-04-25 16:33:06 -0300

    incluindo-testes

---
+++ test/test.py
@@ -0,0 +1,18 @@
+import pytest
+from src.main import *
+
+def test_somar():
+    assert somar(2, 3) == 5
+
+def test_subtrair():
+    assert subtrair(3, 2) == 1
+
+def test_multiplicar():
+    assert multiplicar(4, 3) == 12
+
+def test_dividir():
+    assert dividir(10, 2) == 5
+
+def test_dividir_por_zero():
+    with pytest.raises(ValueError):
+        dividir(10, 0)
